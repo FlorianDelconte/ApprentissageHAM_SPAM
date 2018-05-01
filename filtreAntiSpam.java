0823c4be1ccac67cf2131c5fccfc62a5ca066891
Florian <florian.delconte@protonmail.com>
2018-05-01 16:28:36 +0200

    push des bases d'apprentissage et de test + dictionnaire

---
+++ filtreAntiSpam.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class filtreAntiSpam {
+	
+	protected ArrayList<String> dictionnaire;
+	public static void main(String[] args) {
+		//on charge le dictionnaire
+		charger_dictionnaire();
+	}
+	/**
+	 * 
+	 * @return true si l'opération de chargement du dictionnnaire c'est correctement passé
+	 */
+	public static boolean charger_dictionnaire(){
+		return false;
+	}
+}
